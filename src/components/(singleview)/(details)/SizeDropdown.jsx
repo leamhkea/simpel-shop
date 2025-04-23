93ce1c7c538d4870f279aaa51c58999a4be72898
nanajulieaugustine <naje0004@stud.kea.dk>
2025-04-23 12:45:45 +0200

    indsat noget dropdown, ikke stylet

--- src/components/(singleview)/(details)/SizeDropdown.jsx
+++ src/components/(singleview)/(details)/SizeDropdown.jsx
@@ -1,5 +1,14 @@
 const SizeDropdown = () => {
-  return <div></div>;
+  return (
+    <div className="w-100">
+      <select id="cars" name="cars" className="w-100">
+        <option value="volvo">Choose a size</option>
+        <option value="saab">Small</option>
+        <option value="fiat">Medium</option>
+        <option value="audi">Large</option>
+      </select>
+    </div>
+  );
 };
 
 export default SizeDropdown;
