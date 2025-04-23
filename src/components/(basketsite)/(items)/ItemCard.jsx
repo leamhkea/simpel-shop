e9a18b4de147eb7f5d6b3e2ae76e3bdc7fb1a100
leamhkea <lehe0001@stud.kea.dk>
2025-04-23 13:37:36 +0200

    Lavet design på basket items

--- src/components/(basketsite)/(items)/ItemCard.jsx
+++ src/components/(basketsite)/(items)/ItemCard.jsx
@@ -1,17 +1,27 @@
 import ItemQuantity from "./ItemQuantity";
+import SizeDropdown from "@/components/(singleview)/(details)/SizeDropdown";
 
 const ItemCard = () => {
   return (
-    <div>
+    <div className="flex flex-wrap gap-4">
       <img src="placeholder.png" alt="placeholder" />
-      <div className="flex justify-between">
-        <h2>Name on the product</h2>
-        <h3>
-          <strong>DDK 159</strong>
-        </h3>
+
+      {/* Top section */}
+      <div className="flex flex-col justify-between">
+        <div>
+          <div className="flex justify-between flex-wrap">
+            <h2>Name on the product</h2>
+            <h3 className="font-bold">DDK 159</h3>
+          </div>
+          <h3>Brand</h3>
+        </div>
+
+        {/* Bottom section */}
+        <div className="flex justify-between flex-wrap">
+          <ItemQuantity />
+          <SizeDropdown />
+        </div>
       </div>
-      <h3>Brand</h3>
-      <ItemQuantity />
     </div>
   );
 };
