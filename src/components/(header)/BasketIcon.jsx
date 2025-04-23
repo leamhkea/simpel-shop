3dcdf4c61f2a221d264672c5932ff959c158804a
leamhkea <lehe0001@stud.kea.dk>
2025-04-23 10:56:28 +0200

    pathname og icon

--- src/components/(header)/BasketIcon.jsx
+++ src/components/(header)/BasketIcon.jsx
@@ -1,6 +1,6 @@
 import { BsBasket2 } from "react-icons/bs";
 const BasketIcon = () => {
-  return <BsBasket2 />;
+  return <BsBasket2 size={30} />;
 };
 
 export default BasketIcon;
--- src/components/(header)/Navigation.jsx
+++ src/components/(header)/Navigation.jsx
@@ -1,9 +1,36 @@
+"use client";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import BasketIcon from "./BasketIcon";
+
 const Navigation = () => {
+  const pathname = usePathname();
   return (
     <nav>
-      <ul>
-        <li>Home</li>
-        <li>Products</li>
+      <ul className="flex justify-between items-baseline fixed top-0 w-full py-4">
+        <div className="grid gap-8">
+          <Link href="/">
+            <li
+              className={pathname === "/" ? "text-[#F2968F]" : "text-gray-400"}
+            >
+              Home
+            </li>
+          </Link>
+          <Link href="/products">
+            <li
+              className={pathname === "/" ? "text-[#F2968F]" : "text-gray-400"}
+            >
+              Products
+            </li>
+          </Link>
+        </div>
+        <Link href="/basket">
+          <li>
+            <BasketIcon
+              className={pathname === "/" ? "text-[#F2968F]" : "text-gray-400"}
+            />
+          </li>
+        </Link>
       </ul>
     </nav>
   );
