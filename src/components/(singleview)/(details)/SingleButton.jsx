40f4553c7a632fde82935f41aaa832d85188cf1b
nanajulieaugustine <naje0004@stud.kea.dk>
2025-04-24 14:38:40 +0200

    onclick på button

--- src/components/(singleview)/(details)/SingleButton.jsx
+++ src/components/(singleview)/(details)/SingleButton.jsx
@@ -1,8 +1,14 @@
+"use client";
 import { BsBasket2 } from "react-icons/bs";
+import useItemStore from "@/store/itemStore";
 
-const SingleButton = () => {
+const SingleButton = ({ item }) => {
+  const addItem = useItemStore((state) => state.addItem);
   return (
-    <button className="py-3 flex gap-2 bg-(--black) text-(--white) text-center rounded-4xl w-full items-center justify-center mt-5">
+    <button
+      onClick={() => addItem(item)}
+      className="py-3 flex gap-2 bg-(--black) text-(--white) text-center rounded-4xl w-full items-center justify-center mt-5"
+    >
       Add to basket <BsBasket2 size={20} />
     </button>
   );
