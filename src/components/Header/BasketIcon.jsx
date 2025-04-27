d173e388111b18c0212f083fba6c684234fd1ec2
nanajulieaugustine <naje0004@stud.kea.dk>
2025-04-27 21:33:06 +0200

    basket icon statisk

--- src/components/Header/BasketIcon.jsx
+++ src/components/Header/BasketIcon.jsx
@@ -1,7 +1,20 @@
+"use client";
 import { BsBasket2 } from "react-icons/bs";
+import useStore from "@/store/notifStore";
 
 const BasketIcon = () => {
-  return <BsBasket2 size={30} />;
+  const { items } = useStore();
+
+  return (
+    <div className="relative">
+      <BsBasket2 size={30} />
+      {items > 0 && (
+        <span className="absolute -top-1 -right-1 bg-(--grey) text-(--black) rounded-full text-xs w-5 h-5 flex items-center justify-center">
+          {items}
+        </span>
+      )}
+    </div>
+  );
 };
 
 export default BasketIcon;
